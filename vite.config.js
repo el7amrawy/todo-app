622141de5c6975e750a92921374ac15d38f9a6e0
Aly <el7amrawy@outlook.com>
2023-02-05 20:09:32 +0200

    add base to vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import config from "./src/config";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: `/${config.base}`,
+  base: "/todo-app",
 });
